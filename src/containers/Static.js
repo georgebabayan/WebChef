4d8d97cd957fe2e57f77374682e246755994d32c
Louis Rowan <louis.rowan@icloud.com>
2017-03-11 23:02:16 -0800

    adds basic styling to static

--- src/containers/Static.js
+++ src/containers/Static.js
@@ -1,10 +1,16 @@
 const React = require('react')
-const $ = require('jquery')
 
 var data = { 
   "rid": 2,
   "label": "Thirsty Bear",
   "value": "thirsty bear",
+  image: 'https://static1.squarespace.com/static/51ab58f4e4b0361e5f3ed294/51ab58f4e4b0361e5f3ed29a/51ab80bee4b0058e26cfcb1f/1370194240299/Benchmark_Restaurant_Dining_Room_Photographed_by_Evan_Sung.jpg',
+  about: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ',
+  phone: '617-365-1234',
+  address: '21 1st st',
+  email: 'here@there.com',
+  facebook: 'ajsipdja@facebook.com',
+  twitter: '@bananas',
   menu: [
     {
       name: 'steak',
@@ -21,23 +27,36 @@ var data = {
       description: 'eww',
       price: 'free'
     }
-  ]
+  ],
+  styles: {
+    color: 'black',
+    background: 'whitemoke'
+  }
 }
 
 const Static = React.createClass({
   render(){
-
   var menuItems = data.menu.map((item) => {
     return <p key={item.name}>{item.price} {item.name}: {item.description}</p>
   })
 
     return (
-      <div id='staticPage'>
-        <h1>{data.label}</h1>
+      <div id='staticPage' style={data.styles}>
+        <div id='staticLanding' style={{backgroundImage: 'url(' + data.image + ')'}}>
+          <div style={{position: 'absolute', height: '100%', width: '100%', background: 'rgba(0,0,0,.4)'}}></div>
+          <h1>{data.label}</h1>
+        </div>
+        
         <h2>Menu</h2>
         <ul>
           {menuItems}
         </ul>
+
+        <h2>About</h2>
+        {data.about}
+
+
+
       </div>
     )
   }
